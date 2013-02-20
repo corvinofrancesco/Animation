353afee7a6f7532124292122f0994075442b5c2c
Corvino Francesco <corvinofrancesco@alice.it>
2013-02-20 15:00:34 +0100

    Initial import

---
+++ animazioni/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link href="http://www.headjump.de/stylesheets/arrowsandboxes.css" rel="stylesheet" type="text/css" />
+        <script type="text/javascript" src="http://code.jquery.com/jquery-1.4.1.min.js"></script>
+        <script src="http://www.headjump.de/javascripts/jquery_wz_jsgraphics.js" type="text/javascript"></script>
+        <script src="http://www.headjump.de/javascripts/arrowsandboxes.js" type="text/javascript"></script>
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        
+        ?>
+    </body>
+</html>
